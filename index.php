cbdec6641868317339213128bb5c3f676b612f46
alfiomusmarra <alfiomusmarra@gmail.com>
2014-11-06 20:13:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 include 'inc/loader.inc.php'; //carica le funzioni che sono presenti in ogni pagina
 
 include 'inc/menu.inc.php'; //carica la barra con il menu solo se si è loggati
@@ -6,4 +7,5 @@ include 'inc/menu.inc.php'; //carica la barra con il menu solo se si è loggati
 echo "<img width='200' src='img/dharma_orig.jpg' alt='dharma'>";
 
 include'inc/footer.inc.php'; //chiude le connessioni
+
 ?>
